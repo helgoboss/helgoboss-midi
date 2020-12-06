06feab779b44e75a22a2bccb9907c076bc6d0559
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-12-07 00:29:36 +0100

    Prepare version 0.1.2 and fix version references

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(all(not(feature = "std"), not(test)), no_std)]
-#![doc(html_root_url = "https://docs.rs/helgoboss-midi/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/helgoboss-midi/0.1.2")]
 
 //! Interfaces, data structures and utilities for dealing with MIDI messages.
 //!
